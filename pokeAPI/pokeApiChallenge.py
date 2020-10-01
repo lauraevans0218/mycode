531d1027a00e6bb2ca13250e6a24fb89709a5f48
Laura Evans <lauraevans0218@gmail.com>
2020-10-01 15:45:52 +0000

    poke API Challenge

--- pokeAPI/pokeApiChallenge.py
+++ pokeAPI/pokeApiChallenge.py
@@ -2,13 +2,14 @@
 import requests
 
 
-def api_slice():
+def api_slice(json2python):
     choice = input('What pokemon would you like to see: ')
     r = requests.get(f'https://pokeapi.co/api/v2/pokemon-form/{choice}/')
-    poke_pic = r[1].sprites.front_default.json()
+  #  poke_pic = r[1].sprites.front_default.json()
+    poke_pic = json2python['sprites']['front_default']
     return poke_pic
 
 
 if __name__ == '__main__':
-    print(api_slice())
+    print(api_slice(json2python))
 
